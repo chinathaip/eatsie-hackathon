5d67547feb7aaac631f3e2cb69f2f6bd9cbd8819
Khing <cartoonabe@gmail.com>
2022-11-05 17:13:08 +0700

    delete demo code
    delete demo code

--- app/src/main/java/com/stamford/hackathon/data/FoodDataSource.kt
+++ app/src/main/java/com/stamford/hackathon/data/FoodDataSource.kt
@@ -1,10 +1,7 @@
 package com.stamford.hackathon.data
 
-import com.stamford.hackathon.data.model.Coins
 import retrofit2.http.GET
 
 interface FoodDataSource{
 
-    @GET("coins")
-    suspend fun getFood(): Coins
 }
--- app/src/main/java/com/stamford/hackathon/ui/main/MainViewModel.kt
+++ app/src/main/java/com/stamford/hackathon/ui/main/MainViewModel.kt
@@ -1,9 +1,10 @@
 package com.stamford.hackathon.ui.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.stamford.hackathon.data.model.Coins
+import com.stamford.hackathon.data.model.Item
 import com.stamford.hackathon.domain.GetFoodUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,18 +12,27 @@ import kotlinx.coroutines.withContext
 
 class MainViewModel(private val getFoodUseCase: GetFoodUseCase) : ViewModel() {
 
-    val food = MutableLiveData<Coins>()
+    private val _itemListing = MutableLiveData<List<Item>>()
+    val itemListing: LiveData<List<Item>> = _itemListing
+
+
+    private val _retrievedDataFailedEvent = MutableLiveData<String>()
+    val retrievedDataFailedEvent: LiveData<String> = _retrievedDataFailedEvent
 
     init {
-        getFood()
+        getListing()
     }
 
-    fun getFood() {
+    private fun getListing() {
         viewModelScope.launch {
-            val response = withContext(Dispatchers.IO) {
-                getFoodUseCase.invoke()
+            try {
+                val response = withContext(Dispatchers.IO) {
+                    getFoodUseCase.invoke()
+                }
+                _itemListing.value = response.getOrNull()?.items
+            } catch (exception: Exception) {
+                _retrievedDataFailedEvent.value = exception.message
             }
-            food.value = response
         }
     }
 }
