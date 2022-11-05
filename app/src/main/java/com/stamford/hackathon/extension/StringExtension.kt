0e12c3c2a2efa9f939becf9dac706d13d17c5528
chinathaip <92321280+chinathaip@users.noreply.github.com>
2022-11-05 22:45:36 +0700

    add string extension + test android ci (#5)
    * add string extension
    
    * fix android ci

---
+++ app/src/main/java/com/stamford/hackathon/extension/StringExtension.kt
@@ -0,0 +1,13 @@
+package com.stamford.hackathon.extension
+
+fun <P1 : String?, P2 : String?> whenAllNotBlank(p1: P1, p2: P2, func: (P1, P2) -> Unit) {
+    if (!p1.isNullOrBlank() && !p2.isNullOrBlank()) {
+        func.invoke(p1, p2)
+    }
+}
+
+fun String?.whenNotBlank(func: (String) -> Unit) {
+    if (this != null && this.isNotBlank()) {
+        func.invoke(this)
+    }
+}
